0d05bc4592c3c7ef7d2380e4964acfd5b96454bb
Patrick <82187582+patshash@users.noreply.github.com>
2021-06-18 11:31:38 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 //--------------------------------------------------------------------
 // Variables
 variable "onboarding_token" {}
+variable "oauthid" {}
 
 //--------------------------------------------------------------------
 // Modules
 module "onboarding" {
-  source  = "tfe.patrick.azure.hashidemos.io/hashicorp/onboarding/tfe"
+  source  = "tfe.patrick.azure.hashidemos.io/dryrun/onboarding/tfe"
   version = "0.0.11"
 
   hostname = "tfe.patrick.azure.hashidemos.io"
-  oauthid = "ot-3o8GUfKqXFsL3zvA"
+  oauthid = "${var.oauthid}"
   organisation = "patrickorg"
   project_name = "mynewproject"
   token = "${var.onboarding_token}"
